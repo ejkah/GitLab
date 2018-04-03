c51324fbc649370d23cbeb06175e42ea9d3fa637
ejkah <36532586+ejkah@users.noreply.github.com>
2018-04-02 17:08:53 -0700

    Create rpgGame.c

---
+++ rpgGame.c
@@ -0,0 +1,16 @@
+//Contributors
+//Garrett Poppe 3/19/18
+//Erick Cabanban 3/21/18
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+
+void main(void)
+{
+    int decision = 0;
+    while(choice != 99)
+    {
+    
+    }
+}
